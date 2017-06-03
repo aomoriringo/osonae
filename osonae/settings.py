cd735b5f3d8cf20b6eb079e887b6f6d521710c0b
aomoriringo <sel0433@gmail.com>
2017-06-03 13:10:37 +0900

    add osonae app

--- osonae/settings.py
+++ osonae/settings.py
@@ -65,9 +65,11 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
--- osonae/urls.py
+++ osonae/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'osonae.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', views.index, name="index"),
+]
+
