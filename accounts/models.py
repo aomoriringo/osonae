99b54a2bcfde628e731837ced549f59e7316329f
aomoriringo <sel0433@gmail.com>
2017-06-11 02:06:10 +0900

    add bio field to user model

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# class MyUser(models.Model):
-#     username = models.CharField(max_length=30, unique=True)
-#     email = models.CharField(max_length=100)
-
 
 class MyUser(AbstractUser):
+    bio = models.CharField(max_length=1000, default='')
     image_url = models.URLField('画像URL', blank=True)
 
