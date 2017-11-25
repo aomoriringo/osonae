79e53d40e464d2bd507fb9b09ba97c6e8e81c581
aomoriringo <sel0433@gmail.com>
2017-11-25 03:11:01 +0000

    matomofy login

--- home/views.py
+++ home/views.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 @login_required
 def home(request):
-    not_consumed_posts = Post.not_consumed_posts(request.user).order_by('created_at')
+    not_consumed_posts = Post.not_consumed_posts(request.user)
     context = {'posts': not_consumed_posts}
     return render(request, 'home/index.html', context)
 
