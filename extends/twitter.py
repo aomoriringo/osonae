bdb28d3dc5a1ff4daf5c93a5b69f3a96935bbae1
aomoriringo <sel0433@gmail.com>
2017-12-02 17:59:23 +0000

    save twitter icon url bigger

--- extends/twitter.py
+++ extends/twitter.py
@@ -6,11 +6,12 @@ class TwitterOAuth(auth):
     def get_user_details(self, response):
         """Return user details from Twitter account"""
         fullname, first_name, last_name = self.get_user_names(response['name'])
+        icon_url = response.get('profile_image_url', '').replace('_small', '')
         return {'username': response['screen_name'],
                 'display_name': response['name'],
                 'email': response.get('email', ''),
                 'fullname': fullname,
                 'first_name': first_name,
                 'last_name': last_name,
-                'icon_url': response.get('profile_image_url', '')}
+                'icon_url': icon_url}
 
